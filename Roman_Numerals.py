762e6909d47442b235e0652d09c788b7f802046e
Tgunn <Tgunn@T-TECH>
2025-05-24 18:08:15 -0500

    Add project files.

---
+++ Roman_Numerals.py
@@ -0,0 +1,27 @@
+
+print("Roman Numerals")
+num_in = int(input("Please enter from 1-10 and it will be converted to roman numeral."))
+
+if num_in == 1:
+    print("I")
+elif num_in == 2:
+        print("II")
+elif num_in == 3:
+        print("III")
+elif num_in == 4:
+        print("IV")
+elif num_in == 5:
+        print("V")
+elif num_in == 6:
+        print("VI")
+elif num_in == 7:
+        print("VII")
+elif num_in == 8:
+        print("VIII")
+elif num_in == 9:
+        print("IX")
+elif num_in == 10:
+        print("X")
+
+elif num_in != (1-10):
+        print("Error!! Number out of range!")
